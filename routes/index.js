92c2a1441f679eb1347e98d76e50e57824a4c617
michaelmeidenbauer <70448346+michaelmeidenbauer@users.noreply.github.com>
2021-09-28 21:16:38 -0500

    get a couple more things working

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const apiRouter = require('express').Router();
 const jwt = require('jsonwebtoken');
 const itemsRouter = require('./items');
+const userRouter = require('./users');
 require('dotenv').config();
 const { getUserById } = require('../db');
 
@@ -17,19 +18,19 @@ apiRouter.use(async (req, res, next) => {
     const user = await getUserById(userId);
     delete user.password;
     req.user = user;
-    console.log(' request user: ', req.user);
     next();
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 });
 
 apiRouter.get('/cookie', (req, res) => {
-  const token = jwt.sign({ userId: 2 }, JWT_SECRET);
+  const token = jwt.sign({ userId: 1 }, JWT_SECRET);
   res.cookie('token', token, { httpOnly: true });
   res.json('you got cookie?');
 });
 
 apiRouter.use('/items', itemsRouter);
+apiRouter.use('/users', userRouter);
 
 module.exports = apiRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,19 @@
 const userRouter = require('express').Router();
+const { createUser } = require('../db');
 
-userRouter.use()
-
-
-
-
-
+userRouter.post('/', async (req, res, next) => {
+  try {
+    const userToCreate = req.body;
+    console.log('create user request: ', req);
+    const createdUser = await createUser(userToCreate);
+    return res.send(`success! user created: ${createdUser.username}`);
+  } catch (error) {
+    console.log(error);
+    next({
+      name: 'userCreationError',
+      message: 'something went wrong. please check your user information and try again.',
+    });
+  }
+});
 
 module.exports = userRouter;
