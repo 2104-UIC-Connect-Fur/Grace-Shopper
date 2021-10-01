15504128f26d96db2db4516ae5f1d37c3e1fbc6f
Lucas Wakefield <lucas.wakefield@gmial.com>
2021-10-01 17:06:51 -0500

    formatting auto-fix

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import {
-  App
+  App,
 } from './components';
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
