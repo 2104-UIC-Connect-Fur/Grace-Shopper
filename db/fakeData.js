18def673c1afac1e0aecbc82479e515b0e0892e9
michaelmeidenbauer <70448346+michaelmeidenbauer@users.noreply.github.com>
2021-10-19 19:37:19 -0500

    Revert "RIP cat catsby"
    This reverts commit 3f65290845a6e23adc15ba5b8b9cfd474d89dded.

--- db/fakeData.js
+++ db/fakeData.js
@@ -10,6 +10,16 @@ module.exports = {
       zipcode: '93101',
       isAdmin: false,
     },
+    {
+      username: 'catcatsby',
+      password: 'password',
+      firstname: 'sydney',
+      lastname: 'kitteh',
+      email: 'grump@grump.net',
+      phonenumber: '555555555',
+      zipcode: '60615',
+      isAdmin: true,
+    },
   ],
   userpayments: [
     {
